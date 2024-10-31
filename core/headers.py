d8f06191a22497790cd52cd216188825b72d29bf
ItbaArts <ninjahazzama@gmail.com>
2024-11-01 02:06:20 +0800

    Add files via upload

---
+++ core/headers.py
@@ -0,0 +1,67 @@
+headers = {
+    'Accept': 'application/json, text/plain, */*',
+    'Accept-Encoding': 'gzip, deflate, br',
+    'Accept-Language': 'en-GB,en;q=0.9',
+    'Content-Type': 'application/json',
+    'Connection': 'keep-alive',
+    'Host': 'notpx.app',
+    'Origin': 'https://app.notpx.app',
+    'Referer': 'https://app.notpx.app/',
+    'Sec-Fetch-Dest': 'empty',
+    'Sec-Fetch-Mode': 'cors',
+    'Sec-Fetch-Site': 'same-site',
+    'User-Agent': 'Mozilla/5.0 (Linux; Android 14) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.6422.165 Mobile Safari/537.36'
+}
+
+headers_image = {
+    'Accept': '*/*',
+    'Accept-Encoding': 'gzip, deflate, br',
+    'Accept-Language': 'en-GB,en;q=0.9',
+    'Connection': 'keep-alive',
+    'Host': 'static.notpx.app',
+    'Origin': 'https://app.notpx.app',
+    'Referer': 'https://app.notpx.app/',
+    'Sec-Fetch-Dest': 'empty',
+    'Sec-Fetch-Mode': 'cors',
+    'Sec-Fetch-Site': 'same-site',
+    'User-Agent': 'Mozilla/5.0 (Linux; Android 14) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.6422.165 Mobile Safari/537.36'
+}
+
+headers_socket = {
+#     'Accept-Encoding': 'gzip, deflate, br, zstd',
+#     'Accept-Language': 'en-US,en;q=0.9,ru;q=0.8',
+    'Cache-Control': 'no-cache',
+    'Connection': 'Upgrade',
+#     'Host': 'notpx.app',
+    'Origin': 'https://app.notpx.app',
+    'Pragma': 'no-cache',
+    'Sec-Websocket-Extensions': 'permessage-deflate',
+    'Sec-Websocket-Key': '',
+    'Sec-Websocket-Protocol': 'centrifuge-protobuf',
+    'Sec-Websocket-Version': '13',
+    'Upgrade': 'websocket',
+    'User-Agent': 'Mozilla/5.0 (Linux; Android 14) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.6422.165 Mobile Safari/537.36'
+}
+
+headers_notcoin = {
+    'Accept': '*/*',
+    'Accept-Language': 'en-US,en;q=0.5',
+    'Accept-Encoding': 'gzip, deflate, br, zstd',
+    'Connection': 'keep-alive',
+    'Content-Lenght': '0',
+    'Host': 'api.notcoin.tg',
+    'Origin': 'https://webapp.notcoin.tg',
+    'Referer': 'https://webapp.notcoin.tg/',
+    'bypass-tunnel-reminder': 'x',
+    'TE': 'trailers',
+    'Priority': 'u=1, i',
+    'Sec-Fetch-Dest': 'empty',
+    'Sec-Fetch-Mode': 'cors',
+    'Sec-Fetch-Site': 'same-site',
+    'Sec-Ch-Ua': '"Google Chrome";v="125", "Chromium";v="125", "Not.A/Brand";v="24"',
+    'Sec-Ch-Ua-mobile': '?1',
+    'Sec-Ch-Ua-platform': '"Android"',
+    'User-Agent': 'Mozilla/5.0 (Linux; Android 14) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.6422.165 Mobile Safari/537.36',
+    'X-Auth-Token': 'Bearer null',
+    'Bypass-Tunnel-Reminder': 'x'
+}
